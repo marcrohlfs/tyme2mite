0d2e8aaef9c68e3369592a5ed9fc2a20dd50db28
Marc Rohlfs <rohlfs@silpion.de>
2015-08-17 23:31:16 +0200

    Initial implementation for Go command that transfers time entries from Tyme to mite.

--- README.md
+++ README.md
@@ -1,3 +1,83 @@
 # Tyme to Mite
 
 Read time entries from [Tyme](http://tyme-app.com) and send them to [mite](http://mite.yo.lk).
+
+**Please note**: I'm new to [Go](http://golang.org/) and thus not yet familiar with how to write good Go code. To get
+the initial version of the _tyme2mite_ command running as fast as possible, I put all the code into one single function.
+I'll now start to improve things little by little - when I find some time besides my professional work. See _Objectives_
+for (slight) information about further implementation plans.
+
+## Installation
+
+Should be described on the [Go Documentation](http://golang.org/doc/) page ...
+
+### Dependencies
+
+This package depends on some non-bundled packages:
+
+* _github.com/everdev/mack_
+* _github.com/jimlawless/cfg_
+
+They should normally be installed automatically - depending on who the user installs _this_ package.
+
+### Configuration
+
+The _tyme2mite_ command requires a configuration file named ``tyme2mite.cfg`` in the users' home directory. It must
+contain the following configuration entries:
+
+* ``mite_base_url``: The base URL of the _mite_ service. Example: ``https://DEMO.mite.yo.lk`` (``DEMO`` should be
+  substituted with the name of Your company/organisation account).
+* ``mite_api_key``: The API key for user authentication. It has to be activated and displayed in the user settings
+  [https://DEMO.mite.yo.lk/myself](https://DEMO.mite.yo.lk/myself).
+* ``mite_import_active``: Entries will only be sent to _mite_ if this configuration parameter is defined and its value
+  is explicitly set to ``true``. This should prevent unwanted imports to productive _mite_ during test phase.
+
+## Program Arguments
+
+The _tyme2mite_ command can be executed with two program arguments:
+
+1. The _start date_ of the _Tyme_ entries to be transferred to _mite_.
+2. The _end date_ of the _Tyme_ entries to be transferred to _mite_.
+
+If the second argument is omitted, the current date will be used as _end date_. If both arguments are omitted, all
+entries for _yesterday_ and _today_ will be transferred.
+
+The format of the _date_ arguments is ``yyyy-mm-dd``.
+
+## Data Mapping between Tyme and Mite
+
+As _customers_, _projects_ and _services_ are expected to be changed regularly - either in _mite_ or _Tyme_ -
+``tyme2mite`` doesn't work with any mapping description file. Instead, the names of _projects_ and _tasks_ in _Tyme_
+must match a pattern that allows to resolve the regarding _customers_, _projects_ and _services_ in _mite_ by their
+names:
+
+* The pattern for _project_ names is ``[CUSTOMER_NAME] | [PROJECT_NAME]``.
+* There are two patterns for _task_ names: ``[SERVICE_NAME]`` and ``[CUSTOM_TASK_NAME] | [SERVICE_NAME]``. In latter
+  case, the _custom taks name_ will be prepended before the note.
+
+Time entries with the same _date_, _customer_, _project_, _service_ and _custom task name_ will merged into one entry
+before they're sent to _mite_.
+
+## Objectives
+
+* Instead of requiring name patterns in _Tyme_, _tags_ may be assigned to _projects_ and _tasks_. These _tags_ would
+  describe which _customers_, _projects_ and _services_ should be mapped in _mite_. They could look like follows:
+    * ``mite:customer-name=The Customer`` (assigned to a project)
+    * ``mite:project-name=The Poject`` (assigned to a project)
+    * ``mite:service-name=A Service`` (assigned to tasks)
+* Extract _Go_ library for _mite_
+* Extract _Go_ library for _Tyme_
+* Implement mappings for other tools
+
+## References
+
+API references used for implementation:
+
+* [API. Documentation of mite's RESTful web service](http://mite.yo.lk/en/api/index.html)
+* [Tyme Scripting | Tyme](http://tyme-app.com/tyme-scripting)
+
+## Contributions
+
+Contributions are highly appreciated.
+
+**Please** be sure to use the _develop_ branch as parent for Your feature branches and as target for Your pull requests!
---
+++ tyme2mite.go
@@ -0,0 +1,257 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Marc Rohlfs
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+    "time"
+    "encoding/csv"
+    "encoding/xml"
+    "io/ioutil"
+    "net/http"
+    "github.com/everdev/mack"
+    "github.com/jimlawless/cfg"
+)
+
+const TMP_TYME_EXPORT_CSV_FILE_NAME = "tmp-tyme-export.csv"
+const TMP_TYME_EXPORT_CSV_FILE_PATH = "/Users/rohlfs/Downloads/" + TMP_TYME_EXPORT_CSV_FILE_NAME
+
+type TimeEntry struct {
+    XMLName   xml.Name `xml:"time-entry"`
+    Date      string   `xml:"date-at"`
+    Minutes   int      `xml:"minutes"`
+    Note      string   `xml:"note"`
+    ServiceId int      `xml:"service-id"`
+    ProjectId int      `xml:"project-id"`
+}
+
+type Projects struct {
+    XMLName xml.Name  `xml:"projects"`
+    Project []Project `xml:"project"`
+}
+
+type Project struct {
+    XMLName      xml.Name `xml:"project"`
+    Id           int      `xml:"id"`
+    Name         string   `xml:"name"`
+    CustomerId   int      `xml:"customer-id"`
+    CustomerName string   `xml:"customer-name"`
+}
+
+type Services struct {
+    XMLName xml.Name  `xml:"services"`
+    Service []Service `xml:"service"`
+}
+
+type Service struct {
+    Id   int    `xml:"id"`
+    Name string `xml:"name"`
+}
+
+func main() {
+
+    var startDate string
+    if len(os.Args) >= 2 {
+        startDate = os.Args[1]
+    } else {
+        startDate = time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+    }
+
+    var endDate string
+    if len(os.Args) >= 3 {
+        endDate = os.Args[2]
+    } else {
+        endDate = time.Now().Format("2006-01-02")
+    }
+
+    config, err := cfg.LoadNewMap(os.Getenv("HOME") + "/tyme2mite.cfg")
+    checkErr(err)
+    miteImportActive := strings.ToLower(config["mite_import_active"]) == "true"
+    miteBaseUrl := string(config["mite_base_url"])
+    miteApiKey := string(config["mite_api_key"])
+    miteRequestUrlPattern := miteBaseUrl + "/[RESOURCE_ID]?api_key=" + miteApiKey
+    miteTimeEntriesUrl := strings.Replace(miteRequestUrlPattern, "[RESOURCE_ID]", "time_entries.xml", 1)
+    miteProjectsUrl := strings.Replace(miteRequestUrlPattern, "[RESOURCE_ID]", "projects.xml", 1)
+    miteServicesUrl := strings.Replace(miteRequestUrlPattern, "[RESOURCE_ID]", "services.xml", 1)
+
+    if miteImportActive == false {
+        fmt.Print("[DRY RUN] ")
+    }
+    fmt.Println("Transferring time entries from " + startDate + " to " + endDate + " ...")
+
+    projects := Projects{}
+    res, err := http.Get(miteProjectsUrl)
+    checkErr(err)
+    xmlBody, err := ioutil.ReadAll(res.Body)
+    checkErr(err)
+    xml.Unmarshal(xmlBody, &projects)
+    res.Body.Close()
+
+    services := Services{}
+    resSrv, err := http.Get(miteServicesUrl)
+    checkErr(err)
+    xmlBodySrv, err := ioutil.ReadAll(resSrv.Body)
+    checkErr(err)
+    xml.Unmarshal(xmlBodySrv, &services)
+    resSrv.Body.Close()
+
+    err = mack.Tell("Tyme",
+        "set ex to make new export",
+        "set startDate of ex to (date \"" + startDate + "\")",
+        "set endDate of ex to (date \"" + endDate + "\")",
+        "set exportFormat of ex to csv",
+        "set exportFileName of ex to \"" + TMP_TYME_EXPORT_CSV_FILE_NAME + "\"",
+        "save export ex")
+    checkErr(err)
+
+    csvfile, err := os.Open(TMP_TYME_EXPORT_CSV_FILE_PATH)
+    checkErr(err)
+
+    defer csvfile.Close()
+    os.Remove(TMP_TYME_EXPORT_CSV_FILE_PATH)
+
+    csvReader := csv.NewReader(csvfile)
+    checkErr(err)
+
+    csvReader.Comma = ';'
+    csvReader.FieldsPerRecord = -1
+
+    csvColHeaders, err := csvReader.Read()
+    checkErr(err)
+
+    assert("Date", csvColHeaders[0])
+    assert("Project", csvColHeaders[1])
+    assert("Task", csvColHeaders[2])
+    assert("Duration", csvColHeaders[6])
+    assert("Notes", csvColHeaders[9])
+
+    rawCSVdata, err := csvReader.ReadAll()
+    checkErr(err)
+
+    var timeEntries []TimeEntry
+    for _, each := range rawCSVdata {
+
+        date := each[0]
+
+        duration := strings.Split(each[6], ":")
+        hours, err := strconv.Atoi(duration[0])
+        checkErr(err)
+        minutes, err := strconv.Atoi(duration[1])
+        checkErr(err)
+        minutes = hours * 60 + minutes
+
+        var projectId int
+        customerProject := strings.Split(each[1], "|")
+        customer := strings.TrimSpace(customerProject[0])
+        project := strings.TrimSpace(customerProject[1])
+        for idx := 0; idx < len(projects.Project); idx++ {
+            if customer == projects.Project[idx].CustomerName && project == projects.Project[idx].Name {
+                projectId = projects.Project[idx].Id
+                break
+            }
+        }
+
+        var notePrefix string
+        var noteText string
+        var service string
+        taskService := strings.Split(each[2], "|")
+        if len(taskService) > 1 {
+            notePrefix = strings.TrimSpace(taskService[0]) + ": "
+            noteText = each[9]
+            service = strings.TrimSpace(taskService[1])
+        } else {
+            notePrefix = ""
+            noteText = each[9]
+            service = strings.TrimSpace(taskService[0])
+        }
+
+        var serviceId int
+        for idx := 0; idx < len(services.Service); idx++ {
+            if service == services.Service[idx].Name {
+                serviceId = services.Service[idx].Id
+                break
+            }
+        }
+
+        cumulateTimeEntryIndex := -1
+        for idx := 0; idx < len(timeEntries); idx++ {
+            if timeEntries[idx].Date == date && timeEntries[idx].ProjectId == projectId && timeEntries[idx].ServiceId == serviceId {
+                if len(notePrefix) == 0 || strings.HasPrefix(timeEntries[idx].Note, notePrefix) {
+                    cumulateTimeEntryIndex = idx
+                    break;
+                }
+            }
+        }
+
+        if cumulateTimeEntryIndex == -1 {
+            var timeEntry TimeEntry
+            timeEntry.Date = date
+            timeEntry.Minutes = minutes
+            timeEntry.Note = notePrefix + noteText
+            timeEntry.ProjectId = projectId
+            timeEntry.ServiceId = serviceId
+            timeEntries = append(timeEntries, timeEntry)
+        } else {
+            timeEntries[cumulateTimeEntryIndex].Minutes += minutes
+            timeEntries[cumulateTimeEntryIndex].Note = timeEntries[cumulateTimeEntryIndex].Note + ", " + noteText
+        }
+    }
+
+    for idx := 0; idx < len(timeEntries); idx++ {
+
+        xmlBody, err := xml.MarshalIndent(timeEntries[idx], "", "  ")
+        checkErr(err)
+
+        var xmlString = string(xmlBody)
+        fmt.Println(xmlString)
+
+        if miteImportActive {
+            res, err := http.Post(miteTimeEntriesUrl, "application/xml", strings.NewReader(string(xmlBody)))
+            checkErr(err)
+            fmt.Print("Import result: ")
+            fmt.Println(res)
+        }
+
+        fmt.Println()
+    }
+
+    if miteImportActive == false {
+        fmt.Print("[DRY RUN] ")
+    }
+    fmt.Println("Transferred time entries from " + startDate + " to " + endDate)
+}
+
+func assert(expected string, actual string) {
+    if expected != actual {
+        panic("Unexpected column: " + actual)
+    }
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
